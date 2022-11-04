c1aac08be0d528607387d696f8818d6b90d5c9fb
LauraMarielaLopez <116980061+LauraMarielaLopez@users.noreply.github.com>
2022-11-03 20:44:21 -0600

    Segundo Commit

---
+++ Entidades/Servicio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Servicio
+    {
+        public int Codigo { get; set; }
+        public string Descripcion { get; set; }
+       
+        public decimal Precio { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        
+    }
+}
--- Vista/Login.cs
+++ Vista/Login.cs
@@ -1,10 +1,46 @@
 namespace Vista
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
+
+        private void AceptarButton_Click(object sender, EventArgs e)
+        {
+            if (CodigoUsuarioTextBox.Text == String.Empty)
+            {
+                errorProvider1.SetError(CodigoUsuarioTextBox, "Ingrese un código de usuario");
+                CodigoUsuarioTextBox.Focus();
+                return;
+            }
+            errorProvider1.Clear();
+            if (ClaveTextBox.Text == String.Empty)
+            {
+                errorProvider1.SetError(ClaveTextBox, "Ingrese una clave");
+                ClaveTextBox.Focus();
+                return;
+            }
+            errorProvider1.Clear();
+
+            UsuarioDatos userDatos = new UsuarioDatos();
+
+            bool valido = await userDatos.LoginAsync(CodigoUsuarioTextBox.Text, ClaveTextBox.Text);
+
+            if (valido)
+            {
+                Menu formulario = new Menu();
+                Hide();
+                formulario.Show();
+            }
+            else
+            {
+                MessageBox.Show("Datos de usuario incorrectos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+    
     }
 }
